b55861fc8db87e4bdf4105d10893619be90444b1
Rob McDiarmid <rmcdiarmid@konradgroup.com>
2015-02-18 10:56:34 -0500

    experimenting with jspm

---
+++ config.js
@@ -0,0 +1,13 @@
+System.config({
+  "paths": {
+    "*": "*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "rtts-assert": "npm:rtts-assert@0.0.7"
+  }
+});
+
